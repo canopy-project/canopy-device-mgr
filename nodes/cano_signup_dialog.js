ff4398ea37ef4764f5633ef7ff12a12c278ca088
Greg Prisament <gregjp@toruslabs.com>
2014-05-12 23:39:30 -0700

    Work on signup form

--- nodes/cano_signup_dialog.js
+++ nodes/cano_signup_dialog.js
@@ -30,12 +30,12 @@ function CanoSignupDialogNode(params) {
 
     $me = $("\
         <div class=cano-dialog style='width:300px'>\
-            <div class=l>New to <span class=logo-in-text>canopy?</span></div>\
+            <div class=l>New to <span class=logo-in-text>canopy</span>?</div>\
                 <div id=signup_form style='display:none;'>\
                     Choose a username<br><input name=signup_username id=signup_username type=text></input><br><br>\
                     Enter your email address<br><input name=signup_email id=signup_email type=text></input><br><br>\
                     Choose a password<br><input name=password id=password type=password></input><br><br>\
-                    Re-enter your password<br><input name=password id=password type=password></input><br><br>\
+                    Confirm your password<br><input name=password id=password type=password></input><br><br>\
                 </div>\
             <input id='signup_button' type=submit style='display:none' value='CREATE ACCOUNT'></input>\
             <input id=signup_submit type=submit style='margin-top:16px' value='SIGN UP'></input>\
