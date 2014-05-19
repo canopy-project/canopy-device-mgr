f92d50142df53339521cecfd958d2658857ec631
Greg Prisament <gregjp@toruslabs.com>
2014-05-19 08:31:53 -0700

    Add CanoOptionNode item selection widget

--- nodes/cano_option.js
+++ nodes/cano_option.js
@@ -1,20 +1,78 @@
-
-function CanoOptionNode(params) {
+/*
+ *  CanoOptionNode -- Generic node for selecting single item from list.
+ *
+ *  Required Parameters:
+ *
+ *      items: <object>
+ *          List of items.  See below for details.
+ *
+ *  Optional Parameters:
+ *
+ *      normalClass: <string>
+ *          CSS classes to apply to unselected items.
+ *
+ *      selectedClass: <string>
+ *          CSS classes to apply to selected item.
+ *
+ *      outerClass: <string>
+ *          CSS classes to apply to the container div.
+ *
+ *      onSelect: <function>(<int>idx, <object>item)
+ *          Callback that gets called when an item is selected.
+ *
+ *  Item Object Parameters:
+ *
+ *      html: <string|null>
+ *      node: <object|null>
+ *      value: <any type>
+ *
+ */
+function CanoOptionNode(origParams) {
     var self=this,
-        canopy = params.canopyClient,
-        $me;
+        $me,
+        i,
+        numItems,
+        $items = [],
+        params;
 
     $.extend(this, new CanoNode());
+
+    params = $.extend({}, {
+        normalClass: "",
+        selectedClass: "",
+        outerClass: "",
+        onSelect: function() {},
+        layout_css: {}
+    }, origParams);
 
     this.get$ = function() {
         return $me;
     }
 
     this.onLive = function() {
+        for (i = 0; i < numItems; i++) {
+            $items[i].off('click').on('click', function(idx) {
+                return function() {
+                    select(idx);
+                }(i);
+            }
+        }
     }
 
-    $me = $("\
-        <div>cpu\
-        </div>\
-    ");
+    var select = function(idx) {
+        alert("item " + idx + " selected");
+    }
+
+    $me = $("<div>")
+        .addClass(params.outerClass);
+
+    numItems = params.items.length;
+    for (i = 0; i < numItems; i++) {
+        var $item = $("<div>");
+        $item.html(params.items[i].html);
+        // TODO: handle node
+        $item.addClass(params.normalClass);
+        $items.push($item);
+        $me.append($item);
+    }
 }
