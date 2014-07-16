75f24313ddf8d80ac4500c679beee59451d0f437
Greg Prisament <gregjp@toruslabs.com>
2014-07-16 12:34:39 -0700

    Mockup of main page

--- web/nodes/cano_main_page.js
+++ web/nodes/cano_main_page.js
@@ -1,8 +1,15 @@
+
+/*
+ *       <-------------------1004--------------------------------->
+ *       <---256----><--------492-------------><------256--------->
+ *       padding: 6px
+ */
 function CanoMainPageNode(params) {
     var self=this,
         $me,
         canopy = params.canopyClient,
-        dispatcher = params.dispatcher;
+        dispatcher = params.dispatcher,
+        topbarNode;
 
     $.extend(this, new CanoNode());
 
@@ -13,50 +20,93 @@ function CanoMainPageNode(params) {
     this.onLive = function() {
     }
 
-    $me = $("\
-    <div class='cano-topbar-outer'>\
-        <div style='float:right; padding:4px;'>Logout</div>\
-        <div class='cano-topbar-left-section'>\
-            <div class=logo-in-text>Canopy</div>\
+    topbarNode = new CanoTopbarNode({
+        canopyClient: canopy,
+        dispatcher: dispatcher
+    });
+
+    /*$main = $("\
+    <div class=center_channel>\
+        <div class=cano-dialog-flat style='margin:8px'>\
+            <div style='line-height:1.00; font-size:26px; padding-bottom:2px; display:inline-block; text-align:left;>'>\
+                <span class=s>devices<br></span>3\
+            </div>\
+            <div style='line-height:1.00; font-size:26px; margin-left:20px;padding-bottom:2px; display:inline-block; text-align:left;>'>\
+                <span class=s>online<br></span>2\
+            </div>\
+            <div style='line-height:1.00; font-size:26px; margin-left:20px;padding-bottom:2px; display:inline-block; text-align:left;>'>\
+                <span class=s>disconnected<br></span>1\
+            </div>\
         </div>\
-        <div class='cano-topbar-middle-section'>\
-            <div style='padding-bottom:2px; display:inline-block; border-bottom: 2px solid #c00000;'>My Devices</div>\
-            <div style='padding-bottom:2px; margin-left:20px; display:inline-block;'>Settings</div>\
-        </div>\
-    </div>\
     \
-    <div class=cano-dialog-flat style='margin:8px'>\
-        <div style='line-height:1.00; font-size:26px; padding-bottom:2px; display:inline-block; text-align:left;>'>\
-            <span class=s>devices<br></span>3\
+        <div style='margin:8px'>\
+            <img src='http://canopy.link/shutterstock_82034455_fan_h175.jpg' height=64>\
+            <div style='width:180px; display:inline-block;line-height:1.0'><span class=ml>Greg's SmartFan</span><span class=s style='color:#606060'><br>office</span></div>\
+            <br><br>\
+            <img src='http://canopy.link/shutterstock_82034455_fan_h175.jpg' height=64>\
+            <div style='width:180px; display:inline-block;line-height:1.0; background:#d0d0d0; padding:4px;'><span class=ml>Demo RaspPi</span><span class=s style='color:#606060'><br>office</span></div>\
+            <br><br>\
+            <img src='http://canopy.link/shutterstock_82034455_fan_h175.jpg' height=64>\
+            <div style='width:180px; display:inline-block;line-height:1.0'><span class=ml>Demo RaspPi2</span><span class=s style='color:#606060'><br>office</span></div>\
         </div>\
-        <div style='line-height:1.00; font-size:26px; margin-left:20px;padding-bottom:2px; display:inline-block; text-align:left;>'>\
-            <span class=s>online<br></span>2\
-        </div>\
-        <div style='line-height:1.00; font-size:26px; margin-left:20px;padding-bottom:2px; display:inline-block; text-align:left;>'>\
-            <span class=s>disconnected<br></span>1\
-        </div>\
+    \
     </div>\
-\
-    <div style='margin:8px'>\
-        <img src='http://canopy.link/shutterstock_82034455_fan_h175.jpg' height=64>\
-        <div style='width:180px; display:inline-block;line-height:1.0'><span class=ml>Greg's SmartFan</span><span class=s style='color:#606060'><br>office</span></div>\
-        <br><br>\
-        <img src='http://canopy.link/shutterstock_82034455_fan_h175.jpg' height=64>\
-        <div style='width:180px; display:inline-block;line-height:1.0; background:#d0d0d0; padding:4px;'><span class=ml>Demo RaspPi</span><span class=s style='color:#606060'><br>office</span></div>\
-        <br><br>\
-        <img src='http://canopy.link/shutterstock_82034455_fan_h175.jpg' height=64>\
-        <div style='width:180px; display:inline-block;line-height:1.0'><span class=ml>Demo RaspPi2</span><span class=s style='color:#606060'><br>office</span></div>\
-    </div>\
-\
-    <div style='position: absolute; top:45px; left:264px; '>\
-        <div style='width:512px; height:086px; box-shadow: 2px 2px 8px #808080'>\
+    ");*/
+    $left = $("<div class=cano-main_page-left-section></div>");
+    $middle = $("<div style='' class=cano-main_page-middle-section></div>");
+    $right = $("<div class=cano-main_page-right-section></div>");
+
+    $right.append("<div class=cano-dialog-flat style='width:212px;'>RaspPi Connected<hr>Toast Ready!<hr>Welcome to Canopy!</div>");
+
+    $middle.append("\
+        <div style='line-height:1.0;margin-top:16px;display:inline-block; height:64px;'>\
+            <div style='padding:4px; display:inline-block'>\
+                <div class=btn>OFF</div>\
+                <div class=btn_not_selected>SLOW</div>\
+                <div class=btn_not_selected>MED</div>\
+                <div class=btn_not_selected>FAST</div>\
+                <div style='display:inline-block; text-align:center;'>\
+                    47&deg;F\
+                    <br><span class=s>Temperature</span>\
+                </div>\
+                <div style='display:inline-block; text-align:center;'>\
+                    32%\
+                    <br><span class=s>Humidity</span>\
+                </div>\
+            </div>\
         </div>\
-        <div style='margin-top:16px; width:512px; height:086px; box-shadow: 2px 2px 8px #808080'>\
-        </div>\
-        <div style='margin-top:16px; width:512px; height:086px; box-shadow: 2px 2px 8px #808080'>\
-        </div>\
-        <div style='margin-top:16px; width:512px; height:086px; box-shadow: 2px 2px 8px #808080'>\
-        </div>\
-    </div>\
     ");
+
+    $left.append("\
+        <div style='line-height:1.0;margin-top:16px;display:inline-block'>\
+            <img align=top style='border-radius:7px; box-shadow: 1px 1px 3px #a0a0a0'; src=images/robot1_64x64.jpg>\
+            <div style='padding:4px; display:inline-block'>\
+                <span class=n>Greg's SmartFan</span><br>\
+                <span class=s>living room</span>\
+            </div>\
+        </div>\
+        <div style='line-height:1.0;margin-top:16px; display:inline-block'>\
+            <img align=top style='border-radius:7px; box-shadow: 1px 1px 3px #a0a0a0'; src=images/robot1_64x64.jpg>\
+            <div style='padding:4px; display:inline-block'>\
+                <span class=n>RaspPi</span><br>\
+                <span class=s>living room</span>\
+            </div>\
+        </div>\
+        <div style='line-height:1.0;margin-top:16px; display:inline-block'>\
+            <img align=top style='border-radius:7px; box-shadow: 1px 1px 3px #a0a0a0'; src=images/robot1_64x64.jpg>\
+            <div style='color:#a00000; padding:4px; display:inline-block'>\
+                <span class=n>RaspPI2</span><br>\
+                <span class=s>living room</span>\
+            </div>\
+        </div>\
+    ");
+
+    $main = $("<div class=center_channel></div>");
+    $main.append($left);
+    $main.append($middle);
+    $main.append($right);
+
+    $me = $("<div>");
+    topbarNode.appendTo($me);
+    $main.appendTo($me);
 }
---
+++ web/nodes/cano_topbar.js
@@ -0,0 +1,28 @@
+function CanoTopbarNode(params) {
+    var self=this,
+        $me,
+        canopy = params.canopyClient,
+        dispatcher = params.dispatcher;
+
+    $.extend(this, new CanoNode());
+
+    this.get$ = function() {
+        return $me;
+    }
+
+    this.onLive = function() {
+    }
+
+    $me = $("\
+    <div class='cano-topbar-outer'>\
+        <div class=center_channel>\
+            <div class='cano-topbar-left-section'>\
+                <div class=logo-in-text>Canopy</div>\
+            </div><div class='cano-topbar-middle-section'>\
+                <div style='padding-bottom:4px; display:inline-block; border-bottom: 2px solid #c00000;'>My Devices</div>\
+                <div style='padding-bottom:6px; margin-left:20px; display:inline-block;'>Settings</div>\
+            </div><div class='cano-topbar-right-section'>Logout</div>\
+        </div>\
+    </div>\
+    ");
+}
