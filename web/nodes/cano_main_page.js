4cf7b97bc94d931a6ab29362124b82a9110a5e7f
Greg Prisament <gregjp@toruslabs.com>
2014-07-16 14:24:10 -0700

    Continue working on mockup of devices page

--- web/nodes/cano_main_page.js
+++ web/nodes/cano_main_page.js
@@ -59,7 +59,7 @@ function CanoMainPageNode(params) {
     $right.append("<div class=cano-dialog-flat style='width:212px;'>RaspPi Connected<hr>Toast Ready!<hr>Welcome to Canopy!</div>");
 
     $middle.append("\
-        <div style='line-height:1.0;margin-top:16px;display:inline-block; height:64px;'>\
+        <div style='line-height:1.0;margin-top:110px;display:inline-block; height:64px;'>\
             <div style='padding:4px; display:inline-block'>\
                 <div class=btn>OFF</div>\
                 <div class=btn_not_selected>SLOW</div>\
@@ -80,9 +80,10 @@ function CanoMainPageNode(params) {
     $left.append("\
         <div style='line-height:1.0;margin-top:16px;display:inline-block'>\
             <img align=top style='border-radius:7px; box-shadow: 1px 1px 3px #a0a0a0'; src=images/robot1_64x64.jpg>\
-            <div style='padding:4px; display:inline-block'>\
+            <div style='padding:4px; width:160px; display:inline-block'>\
                 <span class=n>Greg's SmartFan</span><br>\
-                <span class=s>living room</span>\
+                <span class=s>This is my SmartFan.</span><br>\
+                <img src=http://c.dryicons.com/images/icon_sets/minimalistica_part_2_icons/png/32x32/add_mail.png>\
             </div>\
         </div>\
         <div style='line-height:1.0;margin-top:16px; display:inline-block'>\
@@ -101,12 +102,46 @@ function CanoMainPageNode(params) {
         </div>\
     ");
 
-    $main = $("<div class=center_channel></div>");
+    $main = $("<div class=center_channel style='margin-top:16px'></div>");
     $main.append($left);
     $main.append($middle);
     $main.append($right);
 
+    $statusBar = $("\
+        <div class=cano-dialog-flat>\
+            <div style='line-height:1.00; font-size:26px; padding-bottom:2px; display:inline-block; text-align:left;>'>\
+                <span class=s>all<br></span>3\
+            </div>\
+            <div style='line-height:1.00; font-size:26px; margin-left:20px;padding-bottom:2px; display:inline-block; text-align:left;>'>\
+                <span class=s>online<br></span>2\
+            </div>\
+            <div style='line-height:1.00; font-size:26px; margin-left:20px;padding-bottom:2px; display:inline-block; text-align:left;>'>\
+                <span class=s>disconnected<br></span>1\
+            </div>\
+        </div>\
+    ").prependTo($left);;
+
+    $submenuOld = $("<div style='width:100%; margin-top:16px; padding: 6px; padding-bottom:0px; border-bottom:2px solid #d0d0d0'>\
+        <div class=center_channel>\
+            <div style='margin-left:256px'>\
+                <div style='display:inline-block; padding-bottom:4px; border-bottom: 2px solid #a00000;'>All (3)</div>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; \
+                Online (2)&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; \
+                Disconnected (1)&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; \
+            </div>\
+        </div>\
+    </div>");
+    $submenu = $("<div style='width:100%; margin-top:16px; padding: 6px; padding-bottom:0px; '>\
+        <div class=center_channel>\
+            <div class=cano-dialog style='width:440px; margin-left:256px'>\
+                <div style='display:inline-block; padding-bottom:4px; border-bottom: 2px solid #a00000;'>All (3)</div>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; \
+                Online (2)&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; \
+                Disconnected (1)&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; \
+            </div>\
+        </div>\
+    </div>");
+
     $me = $("<div>");
     topbarNode.appendTo($me);
+    //$submenu.appendTo($me);
     $main.appendTo($me);
 }
