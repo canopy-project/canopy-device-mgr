642af742e92fcf82b4404d5238867290da440b4c
Greg Prisament <gregjp@toruslabs.com>
2014-08-05 15:12:41 -0700

    Add example canoweb.conf.js file

---
+++ example-config/canoweb.conf.js
@@ -0,0 +1,14 @@
+/*
+ * Canopy Web Application Configuration File.
+ *
+ * DO NOT COMMIT THIS FILE TO GIT.
+ * This file differs for each deployment of the Canopy Web Application.
+ */
+var gCanopyWebAppConfiguration = {
+    autoRefresh: true,
+    cloudHost : "cws.canopy.link",
+    cloudHTTPPort : 80,
+    cloudHTTPSPort : 433,
+    cloudUseHTTPS : true,
+    javascriptClientURL: "http://static.canopy.link/js/latest/canopy_client.js"
+};
