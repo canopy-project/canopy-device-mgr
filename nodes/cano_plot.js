112cff0e582a758c6889eb30137438077a70cabe
Greg Prisament <gregjp@toruslabs.com>
2014-08-06 16:54:41 -0700

    Improvements to dyzon demo

--- nodes/cano_plot.js
+++ nodes/cano_plot.js
@@ -39,12 +39,15 @@ function CanoPlotNode(origParams)
         dataArray.push([
             "Time",
             "Value"
-        ])
+        ]);
         for (i = 1; i < samples.length; i++) {
             t0 = Date.parse(samples[i-1].t);
             t1 = Date.parse(samples[i].t);
-            if (t1 - t0 > 200000)
+            console.log(t0);
+            console.log(t1);
+            if (t1 - t0 > 2000000)
             {
+                console.log("huh?");
                 dataArray.push([
                     new Date(t0),
                     null
@@ -57,12 +60,14 @@ function CanoPlotNode(origParams)
             else
             {
                 if (params.vAxisFormat == "#%") {
+                    console.log(new Date(t1));
                     dataArray.push([
                         new Date(t1),
                         samples[i].v / 100.0
                     ]);
                 }
                 else {
+                    console.log(new Date(t1));
                     dataArray.push([
                         new Date(t1),
                         /*samples[i].v*1.8 + 32*/
@@ -82,12 +87,12 @@ function CanoPlotNode(origParams)
             legend: { position: 'none' },
             fontName : "Source Sans Pro",
             fontSize : 12,
-            hAxis: {textStyle:{color:"black"}, format: "h:mm a", gridlines: {color: 'transparent'}},
-            vAxis: {textStyle:{color:"black"}, format: params.vAxisFormat, gridlines: {color: 'transparent'}},
-            chartArea:{left:50,top:10,width:900,height:80},
+            hAxis: {baselineColor: "#d8d8d8", textStyle:{color:"black"}, format: "h:mm a", gridlines: {color: 'transparent'}},
+            vAxis: {baselineColor: "#000000", textStyle:{color:"black"}, format: params.vAxisFormat, gridlines: {color: 'transparent'}},
+            chartArea:{left:50,top:10,width:660,height:80},
             height: 110,
-            width: 960,
-            backgroundColor: '#f0f5ff',
+            width: 720,
+            backgroundColor: 'transparent',
         };
 
         var chart = new google.visualization.AreaChart($me[0]);
@@ -97,17 +102,17 @@ function CanoPlotNode(origParams)
     $me = $("<div style='color:#000000'>");
 
     /* Load default data for testing */
-    /*this.setTimeseriesData([
-        { t: 0, v: 0},
-        { t: 1, v: 5},
-        { t: 2, v: 8},
-        { t: 3, v: -5},
-        { t: 4, v: 3.4},
-        { t: 5, v: 9.4},
-        { t: 6, v: 4.2},
-        { t: 7, v: -1},
-        { t: 8, v: 1.5},
-    ]);*/
+    this.setTimeseriesData([
+        { t: "2014-08-06T08:01:47Z", v: 0},
+        { t: "2014-08-06T08:11:47Z", v: 5},
+        { t: "2014-08-06T08:21:47Z", v: 8},
+        { t: "2014-08-06T08:31:47Z", v: -5},
+        { t: "2014-08-06T08:41:47Z", v: 3.4},
+        { t: "2014-08-06T08:51:47Z", v: 9.4},
+        { t: "2014-08-06T09:01:47Z", v: 4.2},
+        { t: "2014-08-06T09:11:47Z", v: -1},
+        { t: "2014-08-06T09:21:47Z", v: 1.5},
+    ]);
 }
 
 
--- nodes/demo/dizon.js
+++ nodes/demo/dizon.js
@@ -17,7 +17,8 @@ function DizonDemoPageNode(params) {
     var self=this,
         $me,
         canopy = params.canopyClient,
-        dispatcher = params.dispatcher
+        dispatcher = params.dispatcher,
+        plotNode
     ;
 
     $.extend(this, new CanoNode());
@@ -57,18 +58,28 @@ function DizonDemoPageNode(params) {
             change: updateImage,
             slide: updateImage
         });
+        $("#thermometer").hover(function(){
+                    $("#plotspot").show();
+                },
+                function(){
+                    $("#plotspot").hide();
+                }
+        );
+        plotNode.appendTo($("#plotspot"));
     }
 
-    $me = $("<div class=center_channel>\
+    $me = $("<div class=center_channel style='padding-left:32px;'>\
+        <br>\
+        <br>\
         <br>\
         <span style='color:#a0a0a0;font-size:70px; font-weight:200'>dyzon</span>\
         <span style='color:#000000;font-size:70px; font-weight:200'>AR06</span>\
-        <div style='line-height:1;margin-left:330px; text-align:right; display:inline-block'><span style='color:#808080;font-size:30px; font-weight:300'>My Smart Fan</span><br>Greg's Office</div>\
-        <table cellspacing=0 cellpadding=0><tr><td  align=center valign=top style='font-size:25px;line-height:1; border-right:1px solid #d8d8d8; padding-right:48px;'>\
+        <div style='white-space:nowrap; float:right; solid #a0a0a0;line-height:1; text-align:right; padding-right:32px;'><br><span style='color:#808080;font-size:40px; font-weight:300'>My Smart Fan</span><span style='font-size:30px'><br>Greg's Office</span></div>\
+        <table cellspacing=0 cellpadding=0><tr><td  align=center valign=top style='padding-left:24px; font-size:20px;line-height:1; border-right:2px solid #d8d8d8; padding-right:24px;'>\
             <br><br><img src=http://b.dryicons.com/images/icon_sets/pixelistica_blue_icons/png/64x64/approve.png>\
             <br>\
             Connected\
-            <br><br><img src=http://b.dryicons.com/images/icon_sets/pixelistica_blue_icons/png/64x64/thermometer.png>\
+            <br><br><img id='thermometer' src=http://b.dryicons.com/images/icon_sets/pixelistica_blue_icons/png/64x64/thermometer.png>\
             <br>\
             84&deg;F\
             <br><br><img src=http://b.dryicons.com/images/icon_sets/pixelistica_blue_icons/png/64x64/drop.png>\
@@ -78,13 +89,22 @@ function DizonDemoPageNode(params) {
             <br>\
             Share\
         </td><td style='padding-left:48px' valign=top>\
-            <img id='fan_image' src=images/dyson_fan_off_cropped.jpg width=100%>\
+            <img id='fan_image' src=images/dyson_fan_off_cropped.jpg width=85%>\
             <div style='position:relative'>\
                 <div style='display:inline-block; position:absolute; top:-110px; left:220px;'>\
-                    <div id='slider' style='font-size:30px;display:inline-block; width:320px'></div>\
+                    <div id='slider' style='font-size:30px;display:inline-block; width:380px'></div>\
                     <div style='display:inline-block; margin-left:40px; font-size:40px' id='slider_title'>OFF</div>\
                 </div>\
             </div>\
+            <div style='position:relative'>\
+                <div id=plotspot style='display:none; padding:4px; position:absolute; left:-100px; top:-370px; background:rgba(255, 255, 240, 0.94); border-bottom: 1px solid#30b0b0; border-top:1px solid #30b0b0;'>\
+                </div>\
+            </div>\
         </table>\
-    </div>");
+    </div><br><br><br><br>");
+
+    plotNode = new CanoPlotNode({
+        title: "hi",
+        vAxisFormat: "#%"
+    });
 }
