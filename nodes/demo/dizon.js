6b8f81f1ecf7a36fab43665c5096dd3da16bc922
Greg Prisament <gregjp@toruslabs.com>
2014-08-06 21:55:41 -0700

    Get fan slider demo working on iPhone

--- nodes/demo/dizon.js
+++ nodes/demo/dizon.js
@@ -65,14 +65,39 @@ function DizonDemoPageNode(params) {
                     $("#plotspot").hide();
                 }
         );
+        $("body").on("touchstart", function(ev) {
+            $("#slider").hide();
+        });
+        $("#fan_image").on("touchstart touchmove", function(ev) {
+            ev.preventDefault();
+            var value = ((ev.originalEvent.pageX - 255) / (800 - 255.0)) * 5;
+            if (value < 0)
+                value = 0;
+            if (value > 5)
+                value = 5;
+            value = Math.floor(value);
+            updateImage(null, {value: value});
+        });
+        $("#instructions").on("touchstart touchmove", function(ev) {
+            ev.preventDefault();
+            var value = ((ev.originalEvent.pageX - 255.) / (800. - 255.0)) * 5;
+            if (value < 0)
+                value = 0;
+            if (value > 5)
+                value = 5;
+            value = Math.floor(value);
+            updateImage(null, {value: value});
+        });
         plotNode.appendTo($("#plotspot"));
+
     }
 
     $me = $("<div class=center_channel style='padding-left:32px;'>\
         <br>\
         <br>\
         <br>\
-        <span style='color:#a0a0a0;font-size:70px; font-weight:200'>dyzon</span>\
+        <!--span style='color:#a0a0a0;font-size:70px; font-weight:200'>dyzon</span-->\
+        <div id=dyzon style='display:inline-block; color:#a0a0a0;font-size:70px; font-weight:200'>dyzon</div>\
         <span style='color:#000000;font-size:70px; font-weight:200'>AR06</span>\
         <div style='white-space:nowrap; float:right; solid #a0a0a0;line-height:1; text-align:right; padding-right:32px;'><br><span style='color:#808080;font-size:40px; font-weight:300'>My Smart Fan</span><span style='font-size:30px'><br>Greg's Office</span></div>\
         <table cellspacing=0 cellpadding=0><tr><td  align=center valign=top style='padding-left:24px; font-size:20px;line-height:1; border-right:2px solid #d8d8d8; padding-right:24px;'>\
